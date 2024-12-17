adb456e25d7adf049cb9f946b1c35ac2368a1b9c
ROBOT <tkata12@outlook.jp>
2024-12-17 18:19:35 +0900

    簡易的なレートリミットを実装

--- src/lib/server/Discord/Bot/Bot.message.ts
+++ src/lib/server/Discord/Bot/Bot.message.ts
@@ -1,7 +1,38 @@
+import { PUBLIC_MESSAGE_LATE_LIMIT } from "$env/static/public";
+import { PermissionsBitField } from "discord.js";
+
 import type { Client, Message, OmitPartialGroupDMChannel } from "discord.js";
 
 export class BotMessage {
-    constructor(private readonly client: Client) {}
+    private readonly lateLimit = Number(PUBLIC_MESSAGE_LATE_LIMIT);
+    private users: Record<string, number> = {};
 
-    public async create(message: OmitPartialGroupDMChannel<Message<boolean>>): Promise<void> {}
+    constructor(private readonly client: Client) {
+        setInterval(() => {
+            for (const [userId, count] of Object.entries(this.users)) {
+                if ((count - this.lateLimit) >= 0) {
+                    this.users[userId] = count - this.lateLimit;
+                } else {
+                    delete this.users[userId];
+                }
+            }
+        }, 1000);
+    }
+
+    public async create(message: OmitPartialGroupDMChannel<Message<boolean>>): Promise<void> {
+        if (!message.member) {
+            return;
+        }
+        if (message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+            return;
+        }
+        const userId = message.author.id;
+        if (!this.users[userId]) {
+            this.users[userId] = 0;
+        }
+        this.users[userId]++;
+        if (this.users[userId] >= this.lateLimit) {
+            await message.delete();
+        }
+    }
 }
