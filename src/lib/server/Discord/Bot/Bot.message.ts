5ae3636817b820c763f21947536a9c640f7a1612
ROBOT <tkata12@outlook.jp>
2024-12-18 03:51:07 +0900

    古いメッセージもスパム時に削除するように改善

--- src/lib/server/Discord/Bot/Bot.message.ts
+++ src/lib/server/Discord/Bot/Bot.message.ts
@@ -3,36 +3,59 @@ import { PermissionsBitField } from "discord.js";
 
 import type { Client, Message, OmitPartialGroupDMChannel } from "discord.js";
 
+export type Archive = {
+    guilds: Record<string, Record<string, OmitPartialGroupDMChannel<Message<boolean>>[]>>
+}
+
 export class BotMessage {
     private readonly lateLimit = Number(PUBLIC_MESSAGE_LATE_LIMIT);
-    private users: Record<string, number> = {};
+    private readonly archive: Archive = {
+        guilds: {},
+    };
 
     constructor(private readonly client: Client) {
         setInterval(() => {
-            for (const [userId, count] of Object.entries(this.users)) {
-                if ((count - this.lateLimit) >= 0) {
-                    this.users[userId] = count - this.lateLimit;
-                } else {
-                    delete this.users[userId];
+            for (const [guildId, users] of Object.entries(this.archive.guilds)) {
+                for (const [userId, messages] of Object.entries(users)) {
+                    const count = messages.length - this.lateLimit;
+                    if (count >= 0) {
+                        this.archive.guilds[guildId][userId] = messages.slice(this.lateLimit);
+                    } else {
+                        delete this.archive.guilds[guildId][userId];
+                    }
+                }
+                if (!Object.keys(users).length) {
+                    delete this.archive.guilds[guildId];
                 }
             }
         }, 1000);
     }
 
     public async create(message: OmitPartialGroupDMChannel<Message<boolean>>): Promise<void> {
-        if (!message.member) {
+        if (!(message.guild && message.member)) {
             return;
         }
         if (message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
             return;
         }
+        const guildId = message.guild.id;
         const userId = message.author.id;
-        if (!this.users[userId]) {
-            this.users[userId] = 0;
+        if (!this.archive.guilds[guildId]) {
+            this.archive.guilds[guildId] = {};
         }
-        this.users[userId]++;
-        if (this.users[userId] >= this.lateLimit) {
-            await message.delete();
+        if (!this.archive.guilds[guildId][userId]) {
+            this.archive.guilds[guildId][userId] = [];
+        }
+
+        this.archive.guilds[guildId][userId].push(message);
+        if (this.archive.guilds[guildId][userId].length >= this.lateLimit) {
+            const messages = this.archive.guilds[guildId][userId].concat();
+            delete this.archive.guilds[guildId][userId];
+            for (const archiveMessage of messages) {
+                try {
+                    await archiveMessage.delete();
+                } catch {}
+            }
         }
     }
 }
